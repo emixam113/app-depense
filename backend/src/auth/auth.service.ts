2ceeb054a3603d0d1b6403d6fd3ed314dacafabd
emixam113 <98613290+emixam113@users.noreply.github.com>
2025-05-05 16:18:06 +0200

    add authentification

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,16 +2,18 @@ import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
 import {User} from '../user/entity/user.entity';
-import { SignupDTO } from './DTO/Signup.dto';
-import * as argon2 from 'argon2'
+import {SignUpDto} from './DTO/SignUp.dto';
+import {LoginDto} from './DTO/Login.dto';
+import * as argon2 from 'argon2'; 
+
 
 @Injectable()
-export class AuthService {
-  constructor(
-    @InjectRepository(User)
-    private userRepository: Repository<User>
-  ) {}
-
-  
-
+export class AuthService{
+ constructor(
+  @InjectRepository(User)
+  private userRepository: Repository<User>, 
+  private JwtService: JwtService 
+ ){}
+ 
+ 
 }
